050e3043022c7a532621d47560c48d170686d9ee
Edouard Jubert <edouard.jubert.externe@emeria.eu>
2026-09-15 16:20:05 +0200

    fix(pretty-print): normalize associative boolean expressions

---
+++ crates/pgls_pretty_print/tests/data/multi/bool_expr_associativity.sql
@@ -0,0 +1,23 @@
+CREATE TABLE demo_output AS
+SELECT
+	*
+FROM
+	demo
+	CROSS JOIN (
+		SELECT
+			1 AS seed
+	) t
+WHERE
+	(
+		demo.accounting_class ~ '^[0-9]+$'
+		AND (
+			demo.accounting_class >= '6000'
+			AND demo.accounting_class <= '6629'
+		)
+		OR starts_with(demo.accounting_class, '71')
+	)
+	AND demo.changed_at > demo.created_at;
+
+SELECT * FROM demo WHERE (a OR (b OR c)) AND d;
+
+SELECT * FROM demo WHERE a AND (b OR c);
